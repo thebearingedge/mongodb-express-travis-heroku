b095264755fb59743ab8e6d4fbba49cdea1342e0
Tim Davis <contact@timdav.is>
2016-10-10 15:07:24 -0700

    Adds mongo db integration and users route.

--- test/create-app.test.js
+++ test/create-app.test.js
@@ -1,33 +1,53 @@
 const expect = require('chai').expect
 const request = require('request')
+const MongoClient = require('mongodb').MongoClient
 const createApp = require('../create-app')
 
+const DEFAULT_MONGO_URI = 'mongodb://localhost:27017/mongodb-express-travis-heroku'
 const PORT = process.env.PORT || 3000
 const host = 'http://localhost:' + PORT
 
 describe('createApp', () => {
 
-  let server
+  let db, server
 
   before(done => {
-    const app = createApp()
-    server = app.listen(PORT, done)
+    MongoClient.connect(DEFAULT_MONGO_URI, (err, _db) => {
+      if (err) return done(err)
+      db = _db
+      const app = createApp(db)
+      server = app.listen(PORT, done)
+    })
   })
 
-  after(() => {
+  after(done => {
     server.close()
+    db.close(true, () => done())
   })
 
   describe('GET /', () => {
 
     it('says hello world!', done => {
-      request(host, (err, res, body) => {
+      request.get(host, (err, res, body) => {
         expect(err).to.be.null
         expect(res).to.have.property('statusCode', 200)
         expect(body).to.equal('hello world')
         done()
       })
 
+    })
+
+  })
+
+  describe('POST /users', () => {
+
+    it('creates a user', done => {
+      request.post(host + '/users', { json: { username: 'Foo' } }, (err, res, body) => {
+        expect(err).to.be.null
+        expect(res).to.have.property('statusCode', 201)
+        expect(body).to.have.property('username', 'Foo')
+        done()
+      })
     })
 
   })
